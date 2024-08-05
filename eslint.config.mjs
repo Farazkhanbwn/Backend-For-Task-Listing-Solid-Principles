a2b616918b5a09ba89895c9cacc260ce10cf36bd
Faraz Khan <farazkhanaa2555@gmail.com>
2024-08-05 18:52:40 +0500

    feat: setup husky for commiting in order to write clean code

---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import prettier from 'eslint-plugin-prettier'
+import tsParser from '@typescript-eslint/parser'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+})
+
+export default [
+  ...compat.extends('eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'),
+  {
+    plugins: {
+      prettier,
+    },
+
+    languageOptions: {
+      parser: tsParser,
+    },
+
+    rules: {
+      'prettier/prettier': ['error'],
+      'linebreak-style': ['error', 'unix'],
+      quotes: ['error', 'single'],
+      semi: ['error', 'never'],
+      'no-undef': 'off',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+    },
+  },
+]
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+const x = 20
+const y = 'fakjfld'
+console.log('y is : ', y)
